e6291eee1859257aa92ae8168a841c0a3177a118
AlexRasmussen <ar@cajo-dk>
2026-02-13 20:13:36 +0100

    Fix startup by reading /data/options.json directly; bump to 1.0.2

--- rejseplanen/run.sh
+++ rejseplanen/run.sh
@@ -1,19 +1,38 @@
-#!/usr/bin/env bash
+#!/bin/sh
 set -eu
 
-source /usr/lib/bashio/bashio.sh
+OPTIONS_FILE="/data/options.json"
 
-export REJSEPLANEN_ACCESS_ID="$(bashio::config 'rejseplanen_access_id')"
-export MQTT_HOST="$(bashio::config 'mqtt_host')"
-export MQTT_PORT="$(bashio::config 'mqtt_port')"
-export MQTT_TOPIC="$(bashio::config 'mqtt_topic')"
-export MQTT_USERNAME="$(bashio::config 'mqtt_username')"
-export MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
-export MQTT_CLIENT_ID="$(bashio::config 'mqtt_client_id')"
-export MQTT_QOS="$(bashio::config 'mqtt_qos')"
-export MQTT_TLS="$(bashio::config 'mqtt_tls')"
-export REJSEPLANEN_MAX_JOURNEYS="$(bashio::config 'rejseplanen_max_journeys')"
-export REJSEPLANEN_DURATION="$(bashio::config 'rejseplanen_duration')"
+read_option() {
+  python3 - "$1" "$OPTIONS_FILE" <<'PY'
+import json
+import sys
+
+key = sys.argv[1]
+path = sys.argv[2]
+with open(path, "r", encoding="utf-8") as f:
+    data = json.load(f)
+value = data.get(key, "")
+if isinstance(value, bool):
+    print("true" if value else "false")
+elif value is None:
+    print("")
+else:
+    print(value)
+PY
+}
+
+export REJSEPLANEN_ACCESS_ID="$(read_option rejseplanen_access_id)"
+export MQTT_HOST="$(read_option mqtt_host)"
+export MQTT_PORT="$(read_option mqtt_port)"
+export MQTT_TOPIC="$(read_option mqtt_topic)"
+export MQTT_USERNAME="$(read_option mqtt_username)"
+export MQTT_PASSWORD="$(read_option mqtt_password)"
+export MQTT_CLIENT_ID="$(read_option mqtt_client_id)"
+export MQTT_QOS="$(read_option mqtt_qos)"
+export MQTT_TLS="$(read_option mqtt_tls)"
+export REJSEPLANEN_MAX_JOURNEYS="$(read_option rejseplanen_max_journeys)"
+export REJSEPLANEN_DURATION="$(read_option rejseplanen_duration)"
 
 while true; do
   python3 /app/app.py 8695035 --cat-out Re --compact-data --mqtt-on || true
